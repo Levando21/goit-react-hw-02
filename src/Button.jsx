19bfcdb3de6cc5a00d3ca93bfc9d96249e986a62
Lev <leozolotukhin@gmail.com>
2024-05-16 09:55:10 +0200

    Update Button.jsx

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,21 +1,23 @@
 /** @format */
-
+import { useState } from "react";
 const Button = function App() {
-	let counter = 0;
+	const [counter, setCounter] = useState(0);
 	const handleIncrement = () => {
-		counter++;
+		setCounter(counter + 2);
 		console.log("iNCREMENTED", counter);
 	};
 
 	const handleDecrement = () => {
-		counter--;
+		setCounter(counter - 2);
 		console.log("Decremented", counter);
 	};
 
 	return (
 		<div>
 			<button onClick={handleIncrement}>Increment</button>
+			<p>{counter}</p>
 			<button onClick={handleDecrement}>Decrement</button>
+			<p>{counter}</p>
 		</div>
 	);
 };
